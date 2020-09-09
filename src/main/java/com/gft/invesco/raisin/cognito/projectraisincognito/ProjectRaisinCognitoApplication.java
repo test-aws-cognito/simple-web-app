b2bd07552220fcbb77cc41f33c96cb9bdd7fa8c5
Lukasz Kotynski <lukasz.kotynski@gft.com>
2020-09-09 10:59:25 +0100

    First commit

---
+++ src/main/java/com/gft/invesco/raisin/cognito/projectraisincognito/ProjectRaisinCognitoApplication.java
@@ -0,0 +1,13 @@
+package com.gft.invesco.raisin.cognito.projectraisincognito;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectRaisinCognitoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectRaisinCognitoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gft/invesco/raisin/cognito/projectraisincognito/ProjectRaisinCognitoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gft.invesco.raisin.cognito.projectraisincognito;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectRaisinCognitoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
