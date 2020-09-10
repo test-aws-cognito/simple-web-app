f034153a3e73b27a702d7e43a195317173dabde7
Lukasz Kotynski <lukasz.kotynski@gft.com>
2020-09-10 12:37:21 +0100

    Present server ip and HOSTNAME variable in REST endpoint result

--- src/main/java/com/gft/invesco/raisin/cognito/projectraisincognito/TestRestController.java
+++ src/main/java/com/gft/invesco/raisin/cognito/projectraisincognito/TestRestController.java
@@ -1,8 +1,13 @@
 package com.gft.invesco.raisin.cognito.projectraisincognito;
 
+import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+import java.text.MessageFormat;
 
 @RestController
 @RequestMapping( "/api/test" )
@@ -10,6 +15,22 @@ public class TestRestController {
 
     @GetMapping( "/ping" )
     public String ping() {
-        return "Pong!";
+        return MessageFormat.format( "Pong from {0} ({1})!", getHostIp(), getHostname() );
+    }
+
+    private String getHostname() {
+        String hostname = System.getenv( "HOSTNAME" );
+        return StringUtils.isEmpty( hostname ) ? "HOSTNAME unknown" : hostname;
+    }
+
+    private String getHostIp() {
+        String ip = "(unknown";
+        try {
+            ip = InetAddress.getLocalHost().getHostAddress();
+        } catch ( UnknownHostException e ) {
+            e.printStackTrace();
+        }
+
+        return ip;
     }
 }
