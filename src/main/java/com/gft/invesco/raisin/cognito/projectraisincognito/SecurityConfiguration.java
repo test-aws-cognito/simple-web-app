6d6e72bfa8b75459431ef6a218b6e782fb2ee748
Lukasz Kotynski <lukasz.kotynski@gft.com>
2020-09-15 09:08:31 +0100

    SSL enabled

--- src/main/java/com/gft/invesco/raisin/cognito/projectraisincognito/SecurityConfiguration.java
+++ src/main/java/com/gft/invesco/raisin/cognito/projectraisincognito/SecurityConfiguration.java
@@ -1,8 +1,5 @@
 package com.gft.invesco.raisin.cognito.projectraisincognito;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -12,7 +9,12 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure( HttpSecurity http ) throws Exception {
-        http.csrf()
+        http
+            .csrf()
+            .and()
+            .requiresChannel()
+            .anyRequest()
+            .requiresSecure()
             .and()
             .authorizeRequests()
             .anyRequest().authenticated()
